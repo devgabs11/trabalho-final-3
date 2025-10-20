7d37a5dab9b63f76b10ead459bfce15f753153f3
Gabriel Hengler <gabriel.hengler@escola.pr.gov.br>
2025-10-20 16:59:56 -0300

    Add files via upload

--- menu1.php
+++ menu1.php
@@ -8,24 +8,26 @@ if (session_status() === PHP_SESSION_NONE) {
 <head>
 <style>
 body {
-  margin: 0;
+  background-color: #ffffff; /* fundo branco */
 }
+
 
 ul {
   list-style-type: none;
   margin: 0;
   padding: 0;
-  width: 150px;
-  background-color: #00BFFF;
+  width: 130px;
+  background-color: #f1f1f1;
   position: fixed;
   height: 100%;
   overflow: auto;
 }
 
-li a {
+
+  li a {
   display: block;
   color: black;
-  padding: 16px 36px;
+  padding: 8px 16px;
   text-decoration: none;
 }
 
@@ -35,8 +37,17 @@ li a.active {
 }
 
 li a:hover:not(.active) {
-  background-color: #000000;
-  color: white;
+  background-color: #00BFFF;
+  color: black;
+}
+h2 {
+  color: black
+}
+h3{
+  color: black
+}
+p{
+  color:black
 }
 </style>
 </head>
@@ -44,16 +55,20 @@ li a:hover:not(.active) {
 
 <ul>
   <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">Noticías</a></li>
-  <li><a href="#contact">Eventos</a></li>
-  <li><a href="#about">Esportes</a></li>
-  <li><a href="#about">Cursos</a></li>
-  <li><a href="#about">Sugestões</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
 </ul>
 
 <div style="margin-left:130px;padding:1px 16px;height:1000px;">
+  <h2>Secretaria da juventude e cidadania</h2>
+  <h3>O que nós somos e nossos valores?</h3>
+<p>A Secretaria de Juventude, Cidadania e Migrantes tem como missão coordenar, executar e aprimorar o sistema de gestão das políticas públicas e dos serviços destinados à Juventude, à Cidadania e à População Migrante.</p>
+  <p> Suas ações são fundamentadas nos princípios da participação popular, da descentralização administrativa e do controle social, promovendo uma atuação articulada e democrática.</p>
+   <p> Nesse processo, destaca-se o papel estratégico do Conselho Municipal de Juventude e dos demais Conselhos de Direitos vinculados, que contribuem para a formulação, monitoramento e avaliação das políticas públicas setoriais.</P>
+<p>Entre suas principais atribuições, está o planejamento, a formulação, a orientação e a execução de políticas públicas voltadas à promoção do desenvolvimento humano, à proteção integral, à inclusão social e à integração plena de diversos segmentos historicamente vulnerabilizados. Isso inclui, de forma prioritária, a juventude, os migrantes nacionais e internacionais, as comunidades indígenas, bem como a população negra e outros grupos étnico-raciais.</P>
+<p>Além disso, a Secretaria atua de forma transversal na promoção da igualdade racial, na valorização da diversidade cultural e na prevenção e combate a todas as formas de discriminação, preconceito e violências, reafirmando o compromisso com os direitos humanos, a justiça social e a construção de uma sociedade mais equitativa, plural e inclusiva.</p>
   
- 
 </div>
 
 </body>
